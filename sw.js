276ae2936be9255e9e0add6d8f3b60a2f12eebbd
uncle-jvp <uncle-jvp@users.noreply.github.com>
2026-04-02 02:08:40 +0800

    chore: 更新时间戳和缓存版本 v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'medieval-mercenary-v13';
+const CACHE_NAME = 'medieval-mercenary-v14';
 const ASSETS = ['/', '/index.html', '/style.css', '/game.js'];
 
 self.addEventListener('install', (e) => {
